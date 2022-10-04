ed29a45294dda5496a1dddbfe378b5f9a0d0ad4e
Burkov Evgenii <Burkovevgeniy@yansex.ru>
2022-10-04 17:44:00 +0300

    aded a programm

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] arr1 = new string[3] {"final", "progect", "text"};
+string[] arr2 = new string[arr1.Length];
+void method(string[] arr1, string[] arr2){
+    int count = 0;
+    for (int i = 0; i< arr1.Length; i++){
+        if (arr1[i].Length <= 3){
+            arr2[count] = arr1[i];
+            count = count +1;
+        }
+    }
+}
+
+void printarr(string[] arr){
+    for (int i = 0; i < arr.Length; i++){
+      Console.Write($"{arr[i]} ");    
+    }
+    System.Console.WriteLine();
+}
+method(arr1, arr2);
+printarr(arr2);
